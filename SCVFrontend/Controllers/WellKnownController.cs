5999a6b388a8b38faf73ad5cf9d24d96fdeef595
Carlos Adriano Portes <xcap2000@outlook.com>
2018-07-24 14:19:11 +0000

    Add ACME endpoint

---
+++ SCVFrontend/Controllers/WellKnownController.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SCVFrontend.Controllers
+{
+    public class WellKnownController : Controller
+    {
+        [Route("/.well-known/acme-challenge/YSd9yjchIP5ou3ZaTaTQM4ElshK6TMVsScZ_GDxcUnU")]
+        public IActionResult Index()
+        {
+            return File(Encoding.UTF8.GetBytes("YSd9yjchIP5ou3ZaTaTQM4ElshK6TMVsScZ_GDxcUnU.fCta6qCK-oFf6d_ExTBGy0868GJ2a_r_H8IzWF4UNWk"), "text/plain");
+        }
+    }
+}
